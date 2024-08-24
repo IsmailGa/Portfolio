cd440a6087d99280f685e3eecfcf8919bf286a5b
Isa <129764901+IsmailGa@users.noreply.github.com>
2024-08-24 23:45:03 +0500

    Changed URL of instagram

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -86,7 +86,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/mmmh_1_/"
+                  href="https://www.instagram.com/ii._ssaa_a/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
